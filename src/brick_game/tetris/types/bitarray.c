0d1afd91a9a04df64a2d13474d6a685439087871
andrey-dru-me1 <andrey260503@gmail.com>
2024-06-18 00:20:24 +0700

    Replace bit array with byte array

--- src/brick_game/tetris/types/bitarray.c
+++ src/brick_game/tetris/types/bitarray.c
@@ -3,28 +3,24 @@
 #include <stdlib.h>
 
 void bitarray_create(bitarray_t *bitarray, size_t n) {
-  size_t actual_len = (n - 1) / 8 + 1;
-  bitarray->bits = calloc(actual_len, sizeof(bit_t));
+  bitarray->bits = calloc(n, sizeof(bit_t));
   bitarray->n = n;
 }
 
 bit_t bitarray_get(bitarray_t *bitarray, size_t i) {
-  return (bitarray->bits[i / 8] >> (i % 8)) & 0b1u;
+  return bitarray->bits[i];
 }
 
 void bitarray_set(bitarray_t *bitarray, size_t i) {
-  bitarray->bits[i / 8] |= 1u << (i % 8);
+  bitarray->bits[i] = 1;
 }
 
 void bitarray_reset(bitarray_t *bitarray, size_t i) {
-  bitarray->bits[i / 8] &= ~(1u << (i % 8));
+  bitarray->bits[i] = 0;
 }
 
 void bitarray_set_bit(bitarray_t *bitarray, size_t i, bit_t val) {
-  if (val == 1)
-    bitarray_set(bitarray, i);
-  else if (val == 0)
-    bitarray_reset(bitarray, i);
+  bitarray->bits[i] = val;
 }
 
 void bitarray_remove(bitarray_t *bitarray) { free(bitarray->bits); }
